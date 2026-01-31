a73ef08efb620fadc73c1dceb601e67815e717fa
LuisMSB5 <sandovalluismanuel522@hotmail.com>
2026-01-30 21:30:37 -0400

    Update code.py

--- code.py
+++ code.py
@@ -341,35 +341,4 @@ class MainApp(App):
 #ejecutador del proyecto
 
 if __name__ == '__main__':
-    MainApp().run()'''
-Codigo de LuisMSB 
-'''
-
-
-import kivy
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.lang import Builder
-from kivy.config import Config
-
-#Agregar un tamaño adaptado para celulares, si se abre en computadora, este tendra un limite
-
-Config.set ('graphics', 'width', 720)
-Config.set ('graphics', 'height', 1080)
-
-class Contenedor (GridLayout):
-    None
-
-Builder.load_file ('interface.kv')
-
-class MainApp(App):
-
-    title ="calculadora LuisMSB"
-
-    def build(self):
-        return Contenedor()
-
-if __name__ == '__main__':
     MainApp().run()
-
